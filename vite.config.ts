fe186b7d4a7a037bb58cf9d2a8ce0fcc24e7279d
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-04-09 16:45:35 +0200

    remove vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 export default defineConfig({
   base: '/messenger-app/',
-  plugins: [react(), tsconfigPaths(), viteCommonjs()],
-  optimizeDeps: {
-    include: ['websocket'],
-    esbuildOptions: {
-      plugins: [esbuildCommonjs(['websocket'])],
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
