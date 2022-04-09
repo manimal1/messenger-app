7ed2d3493afcf8fbd2cdd7fa7dbaaa187e9aa698
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-04-09 17:49:17 +0200

    ignore firebase serialization issues

--- src/+ChatApp/ChatApp.tsx
+++ src/+ChatApp/ChatApp.tsx
@@ -7,15 +7,13 @@ import { loggedInUser } from 'store/_slices/loggedInUserSlice';
 const ChatApp: FC = () => {
   // const chatProps = useMultiChatLogic('1bdd43e7-1aea-43ca-8a72-3892fe288e21', 'jmac', 'MySuperSecretSecret');
   const [{ data, loading: isLoading, error }] = useSwapi('people/1');
-  console.log({ data }, { isLoading }, { error });
   const currentUser = useAppSelector(loggedInUser);
-  console.log({ currentUser });
 
   return (
     <>
       {/* <MultiChatWindow {...chatProps} style={{ height: '100vh' }} />
       <MultiChatSocket {...chatProps} /> */}
-      <p>{data.name}</p>
+      {data?.name && <p>{data.name}</p>}
     </>
   );
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,3 +1,5 @@
+import { getFirebase, actionTypes as rrfActionTypes } from 'react-redux-firebase';
+import { constants as rfConstants } from 'redux-firestore';
 import { configureStore } from '@reduxjs/toolkit';
 import loggedInUser from 'store/_slices/loggedInUserSlice';
 
@@ -5,6 +7,31 @@ export const store = configureStore({
   reducer: {
     loggedInUser,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          // just ignore every redux-firebase and react-redux-firebase action type
+          ...Object.keys(rfConstants.actionTypes).map((type) => `${rfConstants.actionsPrefix}/${type}`),
+          ...Object.keys(rrfActionTypes).map((type) => `@@reactReduxFirebase/${type}`),
+          'loggedInUser/addUser',
+        ],
+        ignoredPaths: [
+          'firebase',
+          'firestore',
+          'loggedInUser.user.*',
+          'loggedInUser.user.proactiveRefresh',
+          'loggedInUser.user.auth',
+          'loggedInUser.user.stsTokenManager',
+          'loggedInUser.user.metadata',
+        ],
+      },
+      thunk: {
+        extraArgument: {
+          getFirebase,
+        },
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
